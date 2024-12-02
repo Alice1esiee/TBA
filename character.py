e7d67559836be32562f3702699e633428497008d
Alice LIN <alice.lin@edu.esiee.fr>
2024-12-02 15:30:24 +0000

    ajout perso

---
+++ character.py
@@ -0,0 +1,12 @@
+# Define the Character class.
+class Character():
+    
+    def __init__(self,name,description,current_room,msgs):
+        self.name = name
+        self.description = description
+        self.current_room = current_room
+        self.msgs = msgs
+    
+    def __str__(self):
+        return self.name + " : " + self.description
+        
