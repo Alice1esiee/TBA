a6f93bee77c5bad20216d99f267752399ce7bf71
matthieu-zozor <matthieu.zozor@edu.esiee.fr>
2025-01-17 12:17:14 +0000

    pylint character

--- character.py
+++ character.py
@@ -1,9 +1,14 @@
 # Define the Character class.
+"""
+pour avoir le choix aleatoire de mouvement de pnj
+"""
 import random
-from config import *
+from config import DEBUG
 
 class Character:
-    
+    """
+    class used for the purpose of creating NPCs around the map
+    """
     def __init__(self,name,description,current_room,msgs):
         self.name = name
         self.description = description
@@ -12,31 +17,37 @@ class Character:
         self.list_msgs = []
         self.alive = True
 
-    
     def __str__(self):
         return self.name + " : " + self.description
-        
+
     def move(self):
+        """
+        class method to make NPCs move around the map
+        """
         l=["move", "stay"]
         condition = random.choice(l)
         if DEBUG:
             print(f"{condition}\n")
-        if (condition == "stay"):
+        if condition == "stay":
             return False
-        else:
-            salles_adj = []
-            for salle in self.current_room.exits.values():
-                if salle is not None:
-                    salles_adj.append(salle)
-            next_room = random.choice(salles_adj)
-            del self.current_room.characters[self.name]
-            self.current_room = next_room
-            self.current_room.characters[self.name]=self
-            if DEBUG:
-                print(f"{self.name} went to {self.current_room.name}\n")
-    
+
+        salles_adj = []
+        for salle in self.current_room.exits.values():
+            if salle is not None:
+                salles_adj.append(salle)
+        next_room = random.choice(salles_adj)
+        del self.current_room.characters[self.name]
+        self.current_room = next_room
+        self.current_room.characters[self.name]=self
+        if DEBUG:
+            print(f"{self.name} went to {self.current_room.name}\n")
+        return True
+
     def get_msg(self):
-        if self.alive == False : 
+        """
+        prints what the NPC has to say
+        """
+        if self.alive is False :
             print("...")
             return
         if self.msgs:
